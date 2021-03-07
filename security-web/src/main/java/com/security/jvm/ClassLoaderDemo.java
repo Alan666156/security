d229b3c676e44fb85c924c9090d95b9199a19d70
alan <alan666156@163.com>
2021-03-07 22:35:29 +0800

    类加载器

--- security-web/src/main/java/com/security/jvm/ClassLoaderDemo.java
+++ security-web/src/main/java/com/security/jvm/ClassLoaderDemo.java
@@ -5,10 +5,26 @@ import com.sun.crypto.provider.DESKeyFactory;
 public class ClassLoaderDemo {
 
     public static void main(String[] args) {
+        //获取系统类加载器sun.misc.Launcher$AppClassLoader
+        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+        System.out.println("系统类加载器：" + systemClassLoader);
+
+        //获取其上层：扩展类加载器
+        ClassLoader extClassLoader = systemClassLoader.getParent();
+        System.out.println("扩展类加载器：" + systemClassLoader);
+
+        //根类加载器:获取不到引导类加载器
+        ClassLoader bootstarpClassLoader = extClassLoader.getParent();
+        System.out.println("根类加载器：" + bootstarpClassLoader);
+
+        //String类使用的根类加载器进行加载的，所以这里获取不到
         System.out.println(String.class.getClassLoader());
-        System.out.println(DESKeyFactory.class.getClassLoader().getClass().getName());
-        System.out.println(ClassLoaderDemo.class.getClassLoader().getClass().getName());
-        System.out.println(ClassLoader.getSystemClassLoader().getClass().getName());
+        //扩展类加载器: sun.misc.Launcher$ExtClassLoader
+        //从 java.ext.dirs 系统属性所指定的目录中加载类库，或从 JDK 的安装目录的 jre/lib/ext 子目录下加载类库
+        System.out.println("扩展类加载器" + DESKeyFactory.class.getClassLoader().getClass().getName());
+        System.out.println("自定义类加载器:" + ClassLoaderDemo.class.getClassLoader().getClass().getName());
+
+
     }
 
 }
