71c518ecb66a07efde7750b1db18947b67a8de0e
fuhongxing <fuhongxing@163.com>
2021-03-14 19:46:08 +0800

    update

--- security-web/src/main/java/com/security/thread/ThreadLocalDemo.java
+++ security-web/src/main/java/com/security/thread/ThreadLocalDemo.java
@@ -1,10 +1,13 @@
 package com.security.thread;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * 
  * @author fhx
  * @date 2019年12月6日
  */
-public class ConnThreadLocal {
+public class ThreadLocalDemo {
 	/**
 	 * ThreadLocal为解决多线程程序的并发问题提供了一种新的思路
 	 * 当使用ThreadLocal维护变量时，ThreadLocal为每个使用该变量的线程提供独立的变量副本，所以每一个线程都可以独立地改变自己的副本，而不会影响其它线程所对应的副本。
@@ -15,30 +18,27 @@ public class ConnThreadLocal {
 		threadLocal.set(value);
 	}
 	public void getThreadLocal(){
-		System.out.println(Thread.currentThread().getName() + ":" + this.threadLocal.get());
+		System.out.println(Thread.currentThread().getName() + ":" + threadLocal.get());
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		final ConnThreadLocal ct = new ConnThreadLocal();
+		final ThreadLocalDemo ct = new ThreadLocalDemo();
 		
-		Thread t1 = new Thread(() -> {
+		new Thread(() -> {
 			ct.setThreadLocal("张三");
 			ct.getThreadLocal();
-		}, "t1");
+		}, "t1").start();
 		
-		Thread t2 = new Thread(() -> {
+		new Thread(() -> {
 			try {
-				Thread.sleep(1000);
+				TimeUnit.SECONDS.sleep(1);
 				//这里获取的为null，因为线程间变量独立
 				ct.getThreadLocal();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}, "t2");
-		
-		t1.start();
-		t2.start();
+		}, "t2").start();
 	}
 	
 }
